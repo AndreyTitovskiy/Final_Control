3c17eff64a9c2131fd8fabbbfaaedf91cd4aca36
andrew titovskiy <titovskiy_andrew@mail.ru>
2023-06-19 21:06:18 +0600

    Выводим первоначальный массив

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -31,3 +31,11 @@ for (int i = 0; i < inputArray.Length; i++)
         index++;
     }
 }
+
+Console.Write("[");
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (i < inputArray.Length - 1) Console.Write(inputArray[i] + ", ");
+    else Console.Write(inputArray[i]);
+}
+Console.Write("]");
